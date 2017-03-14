3dffa9268f69bcb8e47a8590fbe344218d9b7f4f
rbarrientos182 <barrientos.isc@gmail.com>
2017-03-13 20:44:08 -0600

    modelaje de tablas a usar

--- paletizador/views.py
+++ paletizador/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Paletizador
+from .models import Deposito
+
+def paletizador_edit(arg):
+    deposito = Deposito.objects.order_by('idDeposito')
+    template = loader.get_template('paletizador.html')
+    title = 'Configuración Paletizador'
+    context = {
+        'deposito' : deposito,
+        'title' : title
+    }
+
+    return HttpResponse(template.render(context, request))
